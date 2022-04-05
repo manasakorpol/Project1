d4b94159e96bd4b01cd429596c107ae1fc012a99
manasagoud <manasakorpol18@gmail.com>
2022-04-05 21:38:48 +0530

    launch site

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,16 +1,21 @@
 package tests;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 
 public class Test1 
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 		WebDriverManager.chromedriver().setup();
-		ChromeDriverManager driver=new ChromeDriverManager();
-
+		ChromeDriver driver=new ChromeDriver();
+		//launch site
+		driver.get("http://www.facebook.com");
+		Thread.sleep(2000);
+		driver.close();
 	}
 
 }
